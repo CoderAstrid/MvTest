103a9557f018e0893159c81928253ae73065716d
CoderAstrid <astrid.wang1229@gmail.com>
2021-10-19 04:32:22 -0600

    init project

---
+++ BarcodeRecogTest/BarcodeRecogTest/BarcodeRecogTest.h
@@ -0,0 +1,32 @@
+﻿
+// BarcodeRecogTest.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CBarcodeRecogTestApp:
+// See BarcodeRecogTest.cpp for the implementation of this class
+//
+
+class CBarcodeRecogTestApp : public CWinApp
+{
+public:
+	CBarcodeRecogTestApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBarcodeRecogTestApp theApp;
